1f4f1f4b5e810077bc238581926f492c5e5b15d3
Naveen <naveenlx111@gmail.com>
2026-07-25 00:47:33 +0530

    Improve dashboard layout

--- widgets/dashboard_widget.py
+++ widgets/dashboard_widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QGridLayout
+from PySide6.QtWidgets import QWidget, QGridLayout, QSizePolicy
 
 from widgets.card_widget import CardWidget
 from utils.date_utils import days_remaining
@@ -16,6 +16,10 @@ class DashboardWidget(QWidget):
 
     def setup_ui(self):
         layout = QGridLayout(self)
+
+        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setHorizontalSpacing(20)
+        layout.setVerticalSpacing(20)
 
         self.countdown_card = CardWidget(
             "Countdown",
@@ -41,24 +45,38 @@ class DashboardWidget(QWidget):
             "Current Focus"
         )
 
+        cards = [
+            self.countdown_card,
+            self.problems_card,
+            self.daily_goal_card,
+            self.topic_card,
+        ]
+
+        for card in cards:
+            card.setSizePolicy(
+                QSizePolicy.Policy.Expanding,
+                QSizePolicy.Policy.Expanding
+            )
+
         layout.addWidget(self.countdown_card, 0, 0)
         layout.addWidget(self.problems_card, 0, 1)
         layout.addWidget(self.daily_goal_card, 1, 0)
         layout.addWidget(self.topic_card, 1, 1)
 
+        layout.setColumnStretch(0, 1)
+        layout.setColumnStretch(1, 1)
+        layout.setRowStretch(0, 1)
+        layout.setRowStretch(1, 1)
+
     def refresh(self):
         self.countdown_card.set_value(
-            days_remaining(
-                self.config.get("target_date")
-            )
+            days_remaining(self.config.get("target_date"))
         )
 
-        remaining_problems = (
+        self.problems_card.set_value(
             self.config.get("total_problems")
             - self.config.get("solved_problems")
         )
-
-        self.problems_card.set_value(remaining_problems)
 
         self.daily_goal_card.set_value(
             self.config.get("daily_goal")
