77a2f14019ee47b6268ded1029b59baf68e46862
Naveen <naveenlx111@gmail.com>
2026-07-25 00:23:06 +0530

    connect dashboard cards to configuration

--- widgets/dashboard_widget.py
+++ widgets/dashboard_widget.py
@@ -1,56 +1,64 @@
 from PySide6.QtWidgets import QWidget, QGridLayout
+
 from widgets.card_widget import CardWidget
 from utils.date_utils import days_remaining
 
+
 class DashboardWidget(QWidget):
 
-    def __init__(self,config):
+    def __init__(self, config):
         super().__init__()
+
         self.config = config
+
         self.setup_ui()
+        self.refresh()
 
     def setup_ui(self):
         layout = QGridLayout(self)
 
-        countdown = CardWidget(
+        self.countdown_card = CardWidget(
             "Countdown",
-            "154",
+            "--",
             "Days Remaining"
         )
 
-        problems = CardWidget(
+        self.problems_card = CardWidget(
             "Problems",
-            "623",
+            "--",
             "Remaining"
         )
 
-        daily_goal = CardWidget(
+        self.daily_goal_card = CardWidget(
             "Daily Goal",
-            "10",
+            "--",
             "Problems Today"
         )
 
-        topic = CardWidget(
+        self.topic_card = CardWidget(
             "Current Topic",
-            "Dynamic Programming",
+            "--",
             "Current Focus"
         )
 
-        layout.addWidget(countdown, 0, 0)
-        layout.addWidget(problems, 0, 1)
-        layout.addWidget(daily_goal, 1, 0)
-        layout.addWidget(topic, 1, 1)
+        layout.addWidget(self.countdown_card, 0, 0)
+        layout.addWidget(self.problems_card, 0, 1)
+        layout.addWidget(self.daily_goal_card, 1, 0)
+        layout.addWidget(self.topic_card, 1, 1)
+
     def refresh(self):
         self.countdown_card.set_value(
-                days_remaining(
-                    self.config.get("target_date")
-                )
+            days_remaining(
+                self.config.get("target_date")
             )
+        )
 
-        self.problems_card.set_value(
+        remaining_problems = (
             self.config.get("total_problems")
             - self.config.get("solved_problems")
         )
+
+        self.problems_card.set_value(remaining_problems)
 
         self.daily_goal_card.set_value(
             self.config.get("daily_goal")
